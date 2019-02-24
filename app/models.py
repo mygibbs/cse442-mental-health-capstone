2bf26d398d41f0dd70dfdee7ac4bc6722ebead64
Mike6908 <mlong6908@hotmail.com>
2019-02-24 13:51:58 -0500

    New Validation And Security Features

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
+from passlib.hash import sha256_crypt
 from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
@@ -9,7 +10,7 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password, method='sha256')
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
