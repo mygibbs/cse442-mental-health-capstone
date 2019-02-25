9a6e8ba6f571ca106569928dd2e1f2fe0bda8a6d
Matthew Gibbs <mygibbs@gmail.com>
2019-02-24 19:21:05 -0500

    Created better app structure. Create_app function now creates instance of application. Blueprints now separate main functionality. Added some error handling in case of 404 or 500 error.

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,49 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
-login.login_view = 'login'
+from config import Config
 
-from app import routes, models
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'auth.login'
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp)
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    if not app.debug and not app.testing:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240,
+                                           backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('App startup')
+
+    return app
+
+
+from app import models
--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -16,6 +17,7 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
 
 @login.user_loader
 def load_user(id):
