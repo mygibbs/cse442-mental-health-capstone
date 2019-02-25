8dd5cf2c6ec2d0298d8428cd50b91bfb35c5d05a
Matthew Gibbs <mygibbs@gmail.com>
2019-02-24 19:21:05 -0500

    Created better app structure. Create_app function now creates instance of application. Blueprints now separate main functionality. Added some error handling in case of 404 or 500 error.

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,52 @@
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+login.login_view = 'auth.login'
+mail = Mail()
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
-login.login_view = 'login'
-mail = Mail(app)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+    mail.init_app(app)
 
-from app import routes, models
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp)
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    if not app.debug and not app.testing:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/app.log', maxBytes=10240,
+                                           backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('App startup')
+
+    return app
+
+
+from app import models
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,43 +1,44 @@
 from flask import render_template, flash, redirect, request, url_for, Response
-from app import app, db
-from app.forms import LoginForm, RegistrationForm
-from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from flask_login import current_user, login_user, logout_user
 from werkzeug.urls import url_parse
 import time
 
-@app.route('/')
-@app.route('/index')
-@login_required
-def index():
-    return render_template('index.html', title = 'Home')
+from app import db
+from app.auth.forms import LoginForm, RegistrationForm
+from app.auth import bp
+from app.models import User
+from app.auth.forms import ResetPasswordRequestForm, ResetPasswordForm
+from app.email import send_password_reset_email
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@bp.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password.')
-            return redirect(url_for('login'))
+            return redirect(url_for('auth.login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = url_for('main.index')
         return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('auth/login.html', title='Sign In', form=form)
 
-@app.route('/logout')
+
+@bp.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('main.index'))
 
-@app.route('/register', methods=['GET', 'POST'])
+
+@bp.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('main.index'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -45,37 +46,37 @@ def register():
         db.session.add(user)
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', title='Register', form=form)
 
-@app.route('/achievements/')
+
+@bp.route('/achievements')
 def achievements():
     return render_template('achievements.html')
 
-@app.route('/progress')
+
+@bp.route('/progress')
 def progress():
     def generate():
-        x=0
+        x = 0
 
-        while x<=100:
+        while x <= 100:
             yield "data:"+str(x) + "\n\n"
-            x=x+2;
+            x = x+2
             time.sleep(0.6)
 
-    return Response(generate(),mimetype='text/event-stream')
+    return Response(generate(), mimetype='text/event-stream')
 
 
-@app.route('/profile')
+@bp.route('/profile')
 def profile():
-	if current_user.is_authenticated:
-		return render_template('profile.html')
-	else:
-		return redirect(url_for('login'))
+    if current_user.is_authenticated:
+        return render_template('profile.html')
+    else:
+        return redirect(url_for('login'))
 
-from app.forms import ResetPasswordRequestForm
-from app.email import send_password_reset_email
 
-@app.route('/reset_password_request', methods=['GET', 'POST'])
+@bp.route('/reset_password_request', methods=['GET', 'POST'])
 def reset_password_request():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -89,9 +90,8 @@ def reset_password_request():
     return render_template('reset_password_request.html',
                            title='Reset Password', form=form)
 
-from app.forms import ResetPasswordForm
 
-@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+@bp.route('/reset_password/<token>', methods=['GET', 'POST'])
 def reset_password(token):
     if current_user.is_authenticated:
         return redirect(url_for('index'))
