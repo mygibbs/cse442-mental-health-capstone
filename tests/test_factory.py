f24a9ef7c89bbf423460c577a2f9c5c2e0b68733
Matthew Gibbs <mygibbs@gmail.com>
2019-02-25 01:44:39 -0500

    Added basic test infrastructure.

---
+++ tests/test_factory.py
@@ -0,0 +1,12 @@
+from flask import url_for
+
+from app import create_app
+
+
+# def test_config():
+#     assert not create_app().testing
+#     assert create_app({'TESTING': True}).testing
+
+
+# def test_index(client):
+#     assert client.get(url_for('main.index')).status_code == 200
