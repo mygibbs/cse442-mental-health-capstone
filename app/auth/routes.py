f9de9749831cb79e8da830a532ea1fa918e0e9fd
Mike6908 <mlong6908@hotmail.com>
2019-03-03 18:50:18 -0500

    Removed Mail Stuff To Separate Branch - Removed Flask Mail - - Form defined in forms.py, functions in models.py

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -7,8 +7,7 @@ from app import db
 from app.auth.forms import LoginForm, RegistrationForm
 from app.auth import bp
 from app.models import User
-from app.auth.forms import ResetPasswordRequestForm, ResetPasswordForm
-from app.auth.email import send_password_reset_email
+
 
 
 @bp.route('/login', methods=['GET', 'POST'])
@@ -76,32 +75,3 @@ def profile():
         return redirect(url_for('auth.login'))
 
 
-@bp.route('/reset_password_request', methods=['GET', 'POST'])
-def reset_password_request():
-    if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
-    form = ResetPasswordRequestForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user:
-            send_password_reset_email(user)
-        flash('Check Your Email For The Instructions To Reset Your Password')
-        return redirect(url_for('auth.login'))
-    return render_template('reset_password_request.html',
-                           title='Reset Password', form=form)
-
-
-@bp.route('/reset_password/<token>', methods=['GET', 'POST'])
-def reset_password(token):
-    if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
-    user = User.verify_reset_password_token(token)
-    if not user:
-        return redirect(url_for('main.index'))
-    form = ResetPasswordForm()
-    if form.validate_on_submit():
-        user.set_password(form.password.data)
-        db.session.commit()
-        flash('Your password has been reset.')
-        return redirect(url_for('auth.login'))
-    return render_template('email/reset_password.html', form=form)
