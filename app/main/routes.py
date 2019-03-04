3aaa40afc497aecb1bc977aac6fe30d88819bcfc
Mike6908 <mlong6908@hotmail.com>
2019-03-04 17:41:22 -0500

    Reverting changes

--- app/main/routes.py
+++ app/main/routes.py
@@ -17,18 +17,3 @@ def about():
 @login_required
 def achievements():
     return render_template('achievements.html', title='Achivements')
-
-@bp.route('/exercises')
-@login_required
-def exercises():
-    return render_template('exercises.html', title='Exercises')
-
-@bp.route('/profile')
-@login_required
-def profile():
-    return render_template('profile.html', title='Profile')
-	
-@bp.route('/activity1')
-@login_required
-def activity1():
-    return render_template('activity1.html', title='Activity 1')
