c23efcedd7d41f9290979effc709a97d6bd98cf5
Matthew Gibbs <mygibbs@gmail.com>
2019-03-06 10:04:57 -0500

    Revert "Revert "Merge pull request #36 from Mike6908/remove_mail""
    This reverts commit 617363e4798dfe311d0f62c78dfe3f9d1b53f4d1. Reverting Michael's reversion that blew away the repo.

--- README.md
+++ README.md
@@ -11,3 +11,29 @@
 
 ## Trello Scrum Board -> https://trello.com/b/8MU2Zrmh
 ## Task Tracker -> Github Issues
+
+# Workflow
+
+## Step 1: Move Trello card from Things to Do to Doing
+
+## Step 2: Create an issue on Github, assign to yourself
+
+## Step 3: Enter git checkout -b <name-of-branch> develop
+-Creates the branch <name-of-branch> off of the develop branch.
+  
+## Step 4: // Work on branch...
+  
+## Step 5: Enter git add .
+-Adds code to staging area.
+
+## Step 6: Enter git commit -a -m "<Your message here.>"
+-Adds code to local repo.
+  
+## Step 7: Enter push origin <name-of-branch>
+-Pushes code to <name-of-branch> on remote repo.
+  
+## Step 8: Create a pull request. Change base to develop. DON'T PUSH TO MASTER.
+
+## Step 9: Assign task to someone for review.
+
+## Step 10: Once the reviewer has reviewed the code and approved it, merge the pull request into develop.
---
+++ app/auth/routes.py
@@ -0,0 +1,77 @@
+from flask import render_template, flash, redirect, request, url_for, Response
+from flask_login import current_user, login_user, logout_user
+from werkzeug.urls import url_parse
+import time
+
+from app import db
+from app.auth.forms import LoginForm, RegistrationForm
+from app.auth import bp
+from app.models import User
+
+
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password.')
+            return redirect(url_for('auth.login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('main.index')
+        return redirect(next_page)
+    return render_template('auth/login.html', title='Sign In', form=form)
+
+
+@bp.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
+
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', title='Register', form=form)
+
+
+@bp.route('/achievements')
+def achievements():
+    return render_template('achievements.html')
+
+
+@bp.route('/progress')
+def progress():
+    def generate():
+        x = 0
+
+        while x <= 100:
+            yield "data:" + str(x) + "\n\n"
+            x = x + 2
+            time.sleep(0.6)
+
+    return Response(generate(), mimetype='text/event-stream')
+
+
+@bp.route('/profile')
+def profile():
+    if current_user.is_authenticated:
+        return render_template('profile.html')
+    else:
+        return redirect(url_for('auth.login'))
+
+
