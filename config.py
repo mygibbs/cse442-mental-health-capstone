cbae02e833819eb64a8482bcd3f3525a96d762ea
Matthew Gibbs <mygibbs@gmail.com>
2019-03-06 01:21:02 -0500

    Minor changes in preparation for deployment. Removing .flaskenv, each developer must manage their flask environment variables on their own; .gitignore now reflects this change. Configfile has minor changes to protect recaptcha public/private key. Requirements.txt has been updated.

--- config.py
+++ config.py
@@ -6,11 +6,14 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'k82S*P41KUIQfyKb'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or \
+         'a-really-long-and-unique-key-that-nobody-knows'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
-    RECAPTCHA_PUBLIC_KEY = "6LdAkpMUAAAAAHd_N2lqr2xausLbKXPp5zsZ-Zq_"
-    RECAPTCHA_PRIVATE_KEY = "6LdAkpMUAAAAAPBBuNn0C67W44zizgSidd415PhO"
+    RECAPTCHA_PUBLIC_KEY = os.environ.get('RECAPTCHA_PUBLIC_KEY') or \
+        '6LdAkpMUAAAAAHd_N2lqr2xausLbKXPp5zsZ-Zq_'
+    RECAPTCHA_PRIVATE_KEY = os.environ.get('RECAPTCHA_PRIVATE_KEY') or \
+        '6LdAkpMUAAAAAPBBuNn0C67W44zizgSidd415PhO'
     RECAPTCHA_DATA_ATTRS = {'theme': 'dark'}
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
@@ -18,6 +21,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['mygibbs@gmail.com']
+    ADMINS = os.environ.get('ADMINS') or ['mygibbs@gmail.com']
     # TESTING = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
