c88283e8d680c9e5e0a93007a755c10c30871328
Mike6908 <mlong6908@hotmail.com>
2019-04-07 14:19:43 -0400

    Added Activity 6 - Fixed side nav bar - Added route in main.routes - Added activity6.html

--- app/main/routes.py
+++ app/main/routes.py
@@ -37,3 +37,8 @@ def activity1():
 @login_required
 def activity4():
     return render_template('activity4.html', title='Activity 4')
+
+@bp.route('/activity6')
+@login_required
+def activity6():
+    return render_template('activity6.html', title='Activity 6')
