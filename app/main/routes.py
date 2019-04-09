cc718c70eccdce12808c9f0d08d4d4dace3e466b
menglanz <menglanz@menglans-MacBook-Air.local>
2019-04-09 18:50:41 -0400

    Added more activities - Added Activity 5 - Added Activity 101 - Added Activity 2 (6 pages)

--- app/main/routes.py
+++ app/main/routes.py
@@ -55,3 +55,48 @@ def activity4():
 @login_required
 def activity6():
     return render_template('activity6.html', title='Activity 6',user=current_user, points = current_user.points)
+
+@bp.route('/activity101')
+@login_required
+def activity101():
+    return render_template('activity101.html', title='Activity 1',user=current_user, points = current_user.points)
+
+@bp.route('/activity2')
+@login_required
+def activity2():
+    return render_template('activity2.html', title='Activity 2',user=current_user, points = current_user.points)
+
+@bp.route('/activity2_classical')
+@login_required
+def activity2_classical():
+    return render_template('activity2_classical.html', title='Classical Music',user=current_user, points = current_user.points)
+
+@bp.route('/activity2_stringed')
+@login_required
+def activity2_stringed():
+    return render_template('activity2_stringed.html', title='String Music',user=current_user, points = current_user.points)
+
+@bp.route('/activity2_piano')
+@login_required
+def activity2_piano():
+    return render_template('activity2_piano.html', title='Music: Piano',user=current_user, points = current_user.points)
+
+@bp.route('/activity2_wind')
+@login_required
+def activity2_wind():
+    return render_template('activity2_wind.html', title='Wind Music',user=current_user, points = current_user.points)
+
+@bp.route('/activity2_natural')
+@login_required
+def activity2_natural():
+    return render_template('activity2_natural.html', title='Natural Sound',user=current_user, points = current_user.points)
+
+@bp.route('/activity2_popular')
+@login_required
+def activity2_popular():
+    return render_template('activity2_popular.html', title='Popular Music',user=current_user, points = current_user.points)
+
+@bp.route('/activity5')
+@login_required
+def activity5():
+    return render_template('activity5.html', title='Activity 5',user=current_user, points = current_user.points)
