99493ed0847bdef96940c3610a1f37bbd352a811
Matthew Gibbs <mygibbs@gmail.com>
2019-05-15 06:07:36 -0400

    Fixed broken Selenium Test, will be adding it to CircleCi workflow in a few miutes.

--- tests/test_ub_mental_health_auth.py
+++ tests/test_ub_mental_health_auth.py
@@ -1,6 +1,7 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class UBMentalHealthAuth(unittest.TestCase):
@@ -8,23 +9,29 @@ class UBMentalHealthAuth(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
 
-    # def test_title(self):
-    #     driver = self.driver
-    #     driver.get("http://www.ubmentalhealth.heroku.org")
-    #     self.assertIn("Sign In - Mental Health App", driver.title)
-    #
-    # def test_register(self):
-    #     driver = self.driver
-    #     driver.get("http://www.ubmentalhealth.heroku.org")
-    #     self.assertIn("Sign In - Mental Health App", driver.title)
-    #     username = driver.find_element_by_name("username")
-    #     username.send_keys("1_test_user_to_rule_them_all")
-    #     password = driver.find_element_by_name("password")
-    #     password.send_keys("g5E483&^s34fEl%W")
-    #     submit = driver.find_element_by_name("submit")
-    #     submit.send_keys(Keys.RETURN)
-    #     self.assertIn("Home - Mental Health App", driver.title)
-    #     assert "Hi,  1_test_user_to_rule_them_all" in driver.page_source
+    def test_title(self):
+        driver = self.driver
+        driver.get("https://ubmentalhealth.herokuapp.com/login?next=%2F")
+        self.assertIn("Sign In - Mental Health App", driver.title)
+
+    def test_register(self):
+        driver = self.driver
+        driver.get("https://ubmentalhealth.herokuapp.com/login?next=%2F")
+        driver.implicitly_wait(5)
+        username = driver.find_element_by_name("username")
+        driver.implicitly_wait(1)
+        username.send_keys("admin_testing")
+        driver.implicitly_wait(1)
+        password = driver.find_element_by_name("password")
+        driver.implicitly_wait(1)
+        password.send_keys("changeme")
+        driver.implicitly_wait(1)
+        submit = driver.find_element_by_name("submit")
+        driver.implicitly_wait(1)
+        submit.send_keys(Keys.RETURN)
+        driver.implicitly_wait(10)
+        # wait = WebDriverWait(driver, 10)
+        assert driver.find_element_by_id("myCarousel").is_displayed()
 
     def tearDown(self):
         self.driver.close()
